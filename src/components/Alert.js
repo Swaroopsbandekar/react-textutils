1f612cc2ffa75a6695c192018ed846d0fa61e040
Swaroopsbandekar <103209524+Swaroopsbandekar@users.noreply.github.com>
2023-05-25 20:39:02 +0530

    alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
 function Alert(props) {
-    const capitalize = (word) =>{
-         const lower = word.toLowerCase();
-         return lower.charAt(0).toUpperCase() + word.slice(1); 
-    }
+  const capitalize = (word) => {
+    const lower = word.toLowerCase();
+    return lower.charAt(0).toUpperCase() + word.slice(1);
+  }
   return (
-   props.alert && <div className= "alert alert-success alert-dismissible fade show" role="alert">
-       <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
-     </div>
+    <div style={{ height :'60px' }}>
+      {props.alert && <div className= { `alert alert-${props.alert.type} alert-dismissible fade show` } role="alert">
+        <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
+      </div>
+      }
+    </div>
   )
 }
 
-export default Alert
+  export default Alert
--- src/components/Text.js
+++ src/components/Text.js
@@ -2,16 +2,28 @@ import React, { useState } from 'react'
 
 export default function Text(props) {
   const handleUpClick = () => {
-    let newText = text.toUpperCase();
-    setText(newText)
-    props.showAlert("Converted to Upper Case !", "succes")
+    if (text) {
+      let newText = text.toUpperCase();
+      setText(newText)
+      props.showAlert("Converted to Upper Case !", "success")
+    }else{
+      props.showAlert("Enter Text to Convert", "warning")
 
-  }
+    }
+     
+    } 
+      
 
   const handleLoClick = () => {
+  if (text) {
     setText(text.toLowerCase())
     props.showAlert("Converted to Lower Case !", "success")
+  }else{
+    props.showAlert("Enter Text to Convert", "warning")
   }
+   
+   }
+  
 
   const handleOnClick = (event) => {
     setText(event.target.value)
@@ -23,21 +35,25 @@ export default function Text(props) {
 
   }
   const handleCpClick = () => {
+  if (text) {
     navigator.clipboard.writeText(text)
     props.showAlert("Copied to Clipboard !", "success")
+  } else{
+    props.showAlert("Enter text to copy !", "warning")
+  }
 
   }
 
 
-  const [text, setText] = useState('Enter Text Here.')
+  const [text, setText] = useState('')
 
   return (
-    <div style={{color: props.mode==='dark'? 'white':'black'}} >
+    <div style={{ color: props.mode === 'dark' ? 'white' : 'black' }} >
 
-      <div className="container mb-3" style={{color: props.mode==='dark'? 'white':'black'}}>
+      <div className="container mb-3" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
         <h2>{props.heading}</h2>
         <label htmlFor="exampleFormControlTextarea1" className="form-label"></label>
-        <textarea className="form-control" style={{backgroundColor: props.mode==='dark'? 'gray':'white', color:props.mode==='dark'? 'white':'black'}} id="exampleFormControlTextarea1" value={text} onChange={handleOnClick} rows="8"></textarea>
+        <textarea className="form-control" style={{ backgroundColor: props.mode === 'dark' ? 'gray' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} id="exampleFormControlTextarea1" value={text} onChange={handleOnClick} rows="8"></textarea>
       </div>
       <button className="btn btn-primary mx-3" onClick={handleUpClick}>Conver To Upper Case</button>
       <button className="btn btn-primary , mx-3 my-2 " onClick={handleLoClick}>Conver To Lower Case</button>
@@ -46,23 +62,18 @@ export default function Text(props) {
 
       <div className="container my-3">
         <h2>Your Text Summary</h2>
-        <p>Words are <b> {text.split(" ").length-1 } </b>|| characters are <b>{text.length }</b> </p> 
+        <p>Words are <b> {text.split(" ").filter((element) => { return element.length !== 0 }).length} </b>|| characters are <b>{text.length}</b> </p>
         <h6>Preview</h6>
-        <p>{text}</p>
+        <p>{text.length > 0 ? text : "Enter Something into textbox to Preview here."}</p>
 
       </div>
       <div className="container my-5">
-        <h6 > <a style={{color: props.mode==='dark'? 'skyblue':'blue', textDecoration: 'none'}} href="/">@created By Swaroop</a> </h6>
-        </div>
+        <h6 > <a style={{ color: props.mode === 'dark' ? 'skyblue' : 'blue', textDecoration: 'none' }} href="/">@created By Swaroop</a> </h6>
+      </div>
 
-       
- 
-   
-     
-    
-</div>
-   
+    </div>
 
-    
+
+
   )
 }
